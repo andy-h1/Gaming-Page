fe28b287f1de0d7586fd919ddb0cf4b6e5cddb5b
Andy Huynh <a.huynh2612@gmail.com>
2020-06-01 19:57:23 +0100

    Added no change in blank input and fixed CSS

--- gaming.js
+++ gaming.js
@@ -1,23 +1,32 @@
-const cardButtons = document.querySelector('.button');
-const streamer = document.querySelector('#sname');
-const link = document.querySelector('#link');
+const addLink = document.querySelector('#addLink');
+const streamerInput = document.querySelector('#sname');
+const linkInput = document.querySelector('#link');
 const streamers = document.querySelector('.streamers');
 
+
 function handleCardButtonClick(event) {
-    console.log(streamer.value);
-    console.log(link.value);
+    console.log(streamerInput.value);
+    console.log(linkInput.value);
+    const validStreamerInput = streamerInput.value;
+    const validLinkInput = linkInput.value;
     const newStreamDetails = `
     <div class="streamer">
-    <p>${streamer.value}</p>
-    <a href="https://twitch.tv/${link.value}" target="_blank">Twitch</a>
+    <p>${validStreamerInput}</p>
+    <a href="https://twitch.tv/${validLinkInput}" target="_blank">Twitch</a>
 </div>
     `;
-
+    if (validStreamerInput === "" || validLinkInput === "") {
+        return false;
+    }
     streamers.insertAdjacentHTML("beforeend", newStreamDetails);
+    event.preventDefault();
 }
 
-cardButtons.addEventListener('click', handleCardButtonClick);
+addLink.addEventListener('click', handleCardButtonClick);
 
+//validate if input is blank
+//if input is blank return error
+//if input is filled out return newStreamer details
 
 
 
